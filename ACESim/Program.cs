1a70005c75edaedf1123ff68031d65bf307c54b1
Michael Abramowicz <abramowicz@collectivejudgment.com>
2017-01-22 10:34:33 -0500

    partially recovered some missing files

--- ACESim/Program.cs
+++ ACESim/Program.cs
@@ -287,7 +287,8 @@ namespace ACESim
                 //string baseOutputDirectory = "C:\\ACESim\\ACESim\\Games\\LitigationGame";
                 //string settingsPath = "C:\\ACESim\\ACESim\\Games\\LitigationGame\\Settings\\LitigationGameMultipleRunSettingsTemp2.xml";
                 string baseOutputDirectory = "C:\\GitHub\\ACESim\\ACESim\\Games\\CurveFittingGame";
-                string settingsPath = "C:\\GitHub\\ACESim\\ACESim\\Games\\CurveFittingGame\\Settings\\OverallSettings1D.xml";
+                //string settingsPath = "C:\\GitHub\\ACESim\\ACESim\\Games\\CurveFittingGame\\Settings\\OverallSettings1D.xml";
+                string settingsPath = "C:\\GitHub\\ACESim\\ACESim\\Games\\LitigationGame\\Settings\\LitigationGameSingleRunSettings.xml";
                 ProgressResumptionOptions progressResumptionOption = ProgressResumptionOptions.ProceedNormallySavingPastProgress; // change this line if some other option is appropriate
                 StartRunning.Go(baseOutputDirectory, settingsPath, new Interactionless(), progressResumptionOption);
             }
