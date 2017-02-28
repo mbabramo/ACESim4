ac89d2c8c924bff487346ede82d36e3bff5d7925
Michael Abramowicz <abramowicz@collectivejudgment.com>
2017-02-28 14:31:12 -0500

    some progress but still always settling at low value

--- ACESim/Games/MyGame/MyGame.cs
+++ ACESim/Games/MyGame/MyGame.cs
@@ -56,7 +56,7 @@ namespace ACESim
             }
             else if (currentDecision.DecisionByteCode == (byte)MyGameDecisions.CourtDecision)
             {
-                MyProgress.CourtRandomSeed = EquallySpaced.GetLocationOfMidpoint(action - 1 /* make zero-based */, MyDefinition.NumCourtSignals);
+                MyProgress.CourtRandomSeed = 1.0 - EquallySpaced.GetLocationOfMidpoint(action - 1 /* make zero-based */, MyDefinition.NumCourtSignals); // by subtracting from 1.0, we make low actions good for the defendant and high actions good for the plaintiff, thus making it more similar to our offers.
                 MyProgress.PWinsAtTrial = (MyProgress.CourtRandomSeed < MyProgress.LitigationQuality); // note that if random seed is equal to litigation quality, plaintiff loses (burden of proof). We thus use an odd number of court random seeds. 
             }
         }
