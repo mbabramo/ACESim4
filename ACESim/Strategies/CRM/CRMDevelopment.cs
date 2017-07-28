c28b6e19f2aebb51917894ba284a562fbd4426b3
mbabramo <mbabramo@yahoo.com>
2017-07-28 17:43:16 -0400

    pure equilibrium working

--- ACESim/Strategies/CRM/CRMDevelopment.cs
+++ ACESim/Strategies/CRM/CRMDevelopment.cs
@@ -1493,7 +1493,8 @@ namespace ACESim
             bool[] player0StrategyEliminated = new bool[player0Permutations];
             bool[] player1StrategyEliminated = new bool[player1Permutations];
             EliminateDominatedStrategies(player0Permutations, player1Permutations, player0Utilities, player1Utilities, player0StrategyEliminated, player1StrategyEliminated);
-            PrintAllEquilibriumStrategies(player0InformationSets, player1InformationSets, player0Permutations, player1Permutations, player0StrategyEliminated, player1StrategyEliminated);
+            List<(int player0Strategy, int player1Strategy)> nashEquilibria = NarrowToNashEquilibria(player0Permutations, player1Permutations, player0Utilities, player1Utilities, player0StrategyEliminated, player1StrategyEliminated, true);
+            PrintAllEquilibriumStrategies(player0InformationSets, player1InformationSets, player0Permutations, player1Permutations, nashEquilibria);
         }
 
         private void PrintMatrix(double[,] arr)
@@ -1511,24 +1512,23 @@ namespace ACESim
             }
         }
 
-        private void PrintAllEquilibriumStrategies(List<(CRMInformationSetNodeTally, int)> player0InformationSets, List<(CRMInformationSetNodeTally, int)> player1InformationSets, int player0Permutations, int player1Permutations, bool[] player0StrategyEliminated, bool[] player1StrategyEliminated)
+        private void PrintAllEquilibriumStrategies(List<(CRMInformationSetNodeTally, int)> player0InformationSets, List<(CRMInformationSetNodeTally, int)> player1InformationSets, int player0Permutations, int player1Permutations, List<(int player0Strategy, int player1Strategy)> nashEquilibria)
         {
             int numPrinted = 0;
             for (int player0StrategyIndex = 0; player0StrategyIndex < player0Permutations; player0StrategyIndex++)
             {
-                if (player0StrategyEliminated[player0StrategyIndex])
-                    continue;
                 SetPureStrategyBasedOnIndex(player0InformationSets, player0StrategyIndex, player0Permutations);
                 for (int player1StrategyIndex = 0; player1StrategyIndex < player1Permutations; player1StrategyIndex++)
                 {
-                    if (player1StrategyEliminated[player1StrategyIndex])
-                        continue;
-                    SetPureStrategyBasedOnIndex(player1InformationSets, player1StrategyIndex, player1Permutations);
-                    Debug.WriteLine($"Player0StrategyIndex {player0StrategyIndex} Player1StrategyIndex {player1StrategyIndex}");
-                    //DEBUGGenerateReports(0, () => "");
-                    //PrintGameTree();
-                    Debug.WriteLine("");
-                    numPrinted++;
+                    if (nashEquilibria.Any(x => x.player0Strategy == player0StrategyIndex && x.player1Strategy == player1StrategyIndex))
+                    {
+                        SetPureStrategyBasedOnIndex(player1InformationSets, player1StrategyIndex, player1Permutations);
+                        Debug.WriteLine($"Player0StrategyIndex {player0StrategyIndex} Player1StrategyIndex {player1StrategyIndex}");
+                        GenerateReports(0, () => "");
+                        //PrintGameTree();
+                        Debug.WriteLine("");
+                        numPrinted++;
+                    }
                 }
             }
             Debug.WriteLine($"Total equilibria: {numPrinted}");
@@ -1536,8 +1536,12 @@ namespace ACESim
 
         private void GetUtilitiesForStrategyCombinations(List<(CRMInformationSetNodeTally, int)> player0InformationSets, List<(CRMInformationSetNodeTally, int)> player1InformationSets, out int player0Permutations, out int player1Permutations, out double[,] player0Utilities, out double[,] player1Utilities)
         {
-            player0Permutations = player0InformationSets.Aggregate(1, (acc, val) => acc * val.Item2);
-            player1Permutations = player1InformationSets.Aggregate(1, (acc, val) => acc * val.Item2);
+            long p0P = player0InformationSets.Aggregate(1L, (acc, val) => acc * (long)val.Item2);
+            long p1P = player1InformationSets.Aggregate(1L, (acc, val) => acc * (long)val.Item2);
+            if (p0P * p1P > 10000000)
+                throw new Exception("Too many combinations.");
+            player0Permutations = (int)p0P;
+            player1Permutations = (int)p1P;
             player0Utilities = new double[player0Permutations, player1Permutations];
             player1Utilities = new double[player0Permutations, player1Permutations];
             for (int player0StrategyIndex = 0; player0StrategyIndex < player0Permutations; player0StrategyIndex++)
@@ -1558,13 +1562,14 @@ namespace ACESim
             bool atLeastOneEliminated = true;
             while (atLeastOneEliminated)
             {
-                atLeastOneEliminated = EliminateDominateStrategies(player0Permutations, player1Permutations, (player0Index, player1Index) => player0Utilities[player0Index, player1Index], player0StrategyEliminated, player1StrategyEliminated);
-                atLeastOneEliminated = atLeastOneEliminated | EliminateDominateStrategies(player1Permutations, player0Permutations, (player1Index, player0Index) => player1Utilities[player0Index, player1Index], player1StrategyEliminated, player0StrategyEliminated);
+                atLeastOneEliminated = EliminateDominatedStrategies(player0Permutations, player1Permutations, (player0Index, player1Index) => player0Utilities[player0Index, player1Index], player0StrategyEliminated, player1StrategyEliminated);
+                atLeastOneEliminated = atLeastOneEliminated | EliminateDominatedStrategies(player1Permutations, player0Permutations, (player1Index, player0Index) => player1Utilities[player0Index, player1Index], player1StrategyEliminated, player0StrategyEliminated);
             }
         }
 
-        private static bool EliminateDominateStrategies(int thisPlayerPermutations, int otherPlayerPermutations, Func<int, int, double> getUtilityFn, bool[] thisPlayerStrategyEliminated, bool[] otherPlayerStrategyEliminated)
+        private static bool EliminateDominatedStrategies(int thisPlayerPermutations, int otherPlayerPermutations, Func<int, int, double> getUtilityFn, bool[] thisPlayerStrategyEliminated, bool[] otherPlayerStrategyEliminated)
         {
+            const bool requireStrictDominance = true;
             bool atLeastOneEliminated = false;
             // compare pairs of strategies by this player to see if one dominates the other
             for (int thisPlayerStrategyIndex1 = 0; thisPlayerStrategyIndex1 < thisPlayerPermutations; thisPlayerStrategyIndex1++)
@@ -1572,7 +1577,7 @@ namespace ACESim
                 {
                     if (thisPlayerStrategyIndex1 == thisPlayerStrategyIndex2 || thisPlayerStrategyEliminated[thisPlayerStrategyIndex1] || thisPlayerStrategyEliminated[thisPlayerStrategyIndex2])
                         continue; // go to next pair to compare
-                    bool index1SometimesBetter = false, index2SometimesBetter = false;
+                    bool index1SometimesBetter = false, index2SometimesBetter = false, sometimesEqual = false;
                     for (int opponentStrategyIndex = 0; opponentStrategyIndex < otherPlayerPermutations; opponentStrategyIndex++)
                     {
                         if (otherPlayerStrategyEliminated[opponentStrategyIndex])
@@ -1580,7 +1585,11 @@ namespace ACESim
                         double thisPlayerStrategyIndex1Utility = getUtilityFn(thisPlayerStrategyIndex1, opponentStrategyIndex);
                         double thisPlayerStrategyIndex2Utility = getUtilityFn(thisPlayerStrategyIndex2, opponentStrategyIndex);
                         if (thisPlayerStrategyIndex1Utility == thisPlayerStrategyIndex2Utility)
-                            continue;
+                        {
+                            sometimesEqual = true;
+                            if (requireStrictDominance)
+                                break;
+                        }
                         if (thisPlayerStrategyIndex1Utility > thisPlayerStrategyIndex2Utility)
                             index1SometimesBetter = true;
                         else
@@ -1588,6 +1597,8 @@ namespace ACESim
                         if (index1SometimesBetter && index2SometimesBetter)
                             break;
                     }
+                    if (requireStrictDominance && sometimesEqual)
+                        continue; // we are not eliminating weakly dominant strategies
                     if (index1SometimesBetter && !index2SometimesBetter)
                     {
                         thisPlayerStrategyEliminated[thisPlayerStrategyIndex2] = true;
@@ -1600,6 +1611,34 @@ namespace ACESim
                     }
                 }
             return atLeastOneEliminated;
+        }
+
+        private static List<(int player0Strategy, int player1Strategy)> NarrowToNashEquilibria(int player0Permutations, int player1Permutations, double[,] player0Utilities, double[,] player1Utilities, bool[] player0StrategyEliminated, bool[] player1StrategyEliminated, bool removePayoffDominatedEquilibria)
+        {
+            // Eliminate any strategy where a player could improve his score by changing strategies.
+            List<int> player0Strategies = player0StrategyEliminated.Select((eliminated, index) => new { eliminated, index }).Where(x => !x.eliminated).Select(x => x.index).ToList();
+            List<int> player1Strategies = player1StrategyEliminated.Select((eliminated, index) => new { eliminated, index }).Where(x => !x.eliminated).Select(x => x.index).ToList();
+            List<(int player0Strategy, int player1Strategy)> candidates = player0Strategies.SelectMany(x => player1Strategies.Select(y => (x, y))).ToList();
+            double[] Payoff(int player0Strategy, int player1Strategy) => new double[] {player0Utilities[player0Strategy, player1Strategy], player1Utilities[player0Strategy, player1Strategy] };
+            bool IsPayoffDominant
+                ((int player0Strategy, int player1Strategy) firstPayoffs, (int player0Strategy, int player1Strategy) secondPayoffs)
+            {
+                bool atLeastOneBetter = player0Utilities[firstPayoffs.player0Strategy, firstPayoffs.player1Strategy] > player0Utilities[secondPayoffs.player0Strategy, secondPayoffs.player1Strategy] || player1Utilities[firstPayoffs.player0Strategy, firstPayoffs.player1Strategy] > player1Utilities[secondPayoffs.player0Strategy, secondPayoffs.player1Strategy];
+                bool neitherWorse = player0Utilities[firstPayoffs.player0Strategy, firstPayoffs.player1Strategy] >= player0Utilities[secondPayoffs.player0Strategy, secondPayoffs.player1Strategy] && player1Utilities[firstPayoffs.player0Strategy, firstPayoffs.player1Strategy] >= player1Utilities[secondPayoffs.player0Strategy, secondPayoffs.player1Strategy];
+                return atLeastOneBetter && neitherWorse;
+            }
+            bool Player0WillChangeStrategy((int player0Strategy, int player1Strategy) candidate)
+            {
+                return player0Strategies.Any(p0 => p0 != candidate.player0Strategy && player0Utilities[p0, candidate.player1Strategy] > player0Utilities[candidate.player0Strategy, candidate.player1Strategy]);
+            };
+            bool Player1WillChangeStrategy((int player0Strategy, int player1Strategy) candidate)
+            {
+                return player1Strategies.Any(p1 => p1 != candidate.player1Strategy && player1Utilities[candidate.player0Strategy, p1] > player1Utilities[candidate.player0Strategy, candidate.player1Strategy]);
+            };
+            var nashEquilibria = candidates.Where(x => !Player0WillChangeStrategy(x) && !Player1WillChangeStrategy(x)).ToList();
+            if (removePayoffDominatedEquilibria)
+                nashEquilibria = nashEquilibria.Where(x => !nashEquilibria.Any(y => IsPayoffDominant(y, x))).ToList();
+            return nashEquilibria;
         }
 
         private void SetPureStrategyBasedOnIndex(List<(CRMInformationSetNodeTally tally, int numPossible)> tallies, int strategyIndex, int totalStrategyPermutations)
