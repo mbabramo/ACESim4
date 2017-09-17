e6a7578d9c1c56cb0b5f0d8482d84865d77e3f87
mbabramo <mbabramo@yahoo.com>
2017-09-17 17:22:35 -0400

    reordered chance decisions

--- ACESim4/Games/MyGame/MyGamePlayers.cs
+++ ACESim4/Games/MyGame/MyGamePlayers.cs
@@ -8,16 +8,24 @@ namespace ACESim
 {
     public enum MyGamePlayers : byte
     {
-        // NOTE: Order: Main players, the resolution player, chance players that may have uneven chance actions (and thus have information sets), then chance players with information sets, then other chance players.
+        // NOTE: Order: Main players, the resolution player, chance players that may have uneven chance actions (and thus have information sets), then chance players with information sets, then other chance players (who don't need to be counted in the MaxNumPlayers with information sets).
         // NOTE2: When adding players, also add in GetPlayersList. Also may need to change values in InformationSetLog and GameHistory, including value for MaxNumPlayers and other values dependent on this.
+
+        // main players (full information sets)
         Plaintiff,
         Defendant,
+
+        // resolution player (full information set)
         Resolution,
-        TrulyLiableChance,
+
+        // chance players (small information sets)
         PNoiseOrSignalChance,
         DNoiseOrSignalChance,
-        CourtChance,
         QualityChance,
+        CourtChance,
+
+        // chance players (no information sets)
+        TrulyLiableChance,
         BothGiveUpChance,
         PreBargainingRoundChance, // no real chance / 1 possible action
         PostBargainingRoundChance, // no real chance / 1 possible action
