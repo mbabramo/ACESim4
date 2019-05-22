13db1c0add976f4586f8c29b001f7f7e0307bb74
Michael Abramowicz <mbabramo@yahoo.com>
2019-05-22 07:58:21 -0400

    MyGameOptionSetChoices

--- ACESimBase/Games/MyGame/Options/MyGameOptionsGenerator.cs
+++ ACESimBase/Games/MyGame/Options/MyGameOptionsGenerator.cs
@@ -9,10 +9,25 @@ namespace ACESim
 {
     public static class MyGameOptionsGenerator
     {
-        static bool ambitious = false;
-        static bool superSimple = true;
+        public enum MyGameOptionSetChoices
+        {
+            SuperSimple,
+            Fast,
+            Usual,
+            Ambitious,
+        }
 
-        public static MyGameOptions SingleRound() => ambitious ? SingleRound_Ambitious() : (superSimple ? SingleRound_SuperSimple() : SingleRound_Usual());
+        static MyGameOptionSetChoices MyGameChoice => MyGameOptionSetChoices.Usual;
+
+        public static MyGameOptions SingleRound() => MyGameChoice switch
+        {
+            MyGameOptionSetChoices.SuperSimple => SingleRound_SuperSimple(),
+            MyGameOptionSetChoices.Fast => SingleRound_Fast(),
+            MyGameOptionSetChoices.Usual => SingleRound_Usual(),
+            MyGameOptionSetChoices.Ambitious => SingleRound_Ambitious(),
+            _ => throw new Exception()
+        };
+
         public static MyGameOptions SingleRound_SuperSimple()
         {
             var options = new MyGameOptions()
@@ -70,7 +85,8 @@ namespace ACESim
             return options;
         }
 
-        public static MyGameOptions SingleRound_Usual()
+
+        public static MyGameOptions SingleRound_Fast()
         {
             var options = new MyGameOptions()
             {
@@ -80,6 +96,63 @@ namespace ACESim
                 NumLitigationQualityPoints = 4,
                 NumSignals = 4,
                 NumOffers = 4,
+                MyGameDisputeGenerator = new MyGameEqualQualityProbabilitiesDisputeGenerator()
+                {
+                    ProbabilityTrulyLiable_LitigationQuality75 = 0.75,
+                    ProbabilityTrulyLiable_LitigationQuality90 = 0.90,
+                    NumPointsToDetermineTrulyLiable = 100,
+                },
+                PFilingCost = 5000,
+                DAnswerCost = 5000,
+                PNoiseStdev = 0.3,
+                DNoiseStdev = 0.3,
+                CourtNoiseStdev = 0.3,
+                CostsMultiplier = 1.0,
+                PTrialCosts = 15_000,
+                DTrialCosts = 15_000,
+                RegretAversion = 0.0,
+                IncludeAgreementToBargainDecisions = false,
+                PerPartyCostsLeadingUpToBargainingRound = 15_000,
+                AllowAbandonAndDefaults = true,
+                LoserPays = false,
+                LoserPaysMultiple = 1.0,
+                LoserPaysAfterAbandonment = false,
+                DeltaOffersOptions = new DeltaOffersOptions()
+                {
+                    SubsequentOffersAreDeltas = false,
+                    DeltaStartingValue = 0.01,
+                    MaxDelta = 0.25
+                },
+                NumPotentialBargainingRounds = 2,
+                BargainingRoundRecall = MyGameBargainingRoundRecall.RememberAllBargainingRounds,
+                SubdivideOffers = false,
+                BargainingRoundsSimultaneous = false,
+                PGoesFirstIfNotSimultaneous = new List<bool> { true, false, true, false, true, false, true, false },
+                IncludeSignalsReport = false,
+                IncludeCourtSuccessReport = false,
+            };
+            // options.AdditionalTableOverrides = new List<(Func<Decision, GameProgress, byte>, string)>() { (MyGameActionsGenerator.GamePlaysOutToTrial, "GamePlaysOutToTrial") };
+            options.PUtilityCalculator = new RiskNeutralUtilityCalculator() { InitialWealth = options.PInitialWealth };
+            options.DUtilityCalculator = new RiskNeutralUtilityCalculator() { InitialWealth = options.DInitialWealth };
+            //options.PUtilityCalculator = new LogRiskAverseUtilityCalculator() { InitialWealth = options.PInitialWealth };
+            //options.DUtilityCalculator = new LogRiskAverseUtilityCalculator() { InitialWealth = options.DInitialWealth };
+            //options.PUtilityCalculator = new CARARiskAverseUtilityCalculator() { InitialWealth = options.PInitialWealth, Alpha = 10 * 0.000001 };
+            //options.DUtilityCalculator = new CARARiskAverseUtilityCalculator() { InitialWealth = options.DInitialWealth, Alpha = 10 * 0.000001 };
+            //options.PUtilityCalculator = new CARARiskAverseUtilityCalculator() { InitialWealth = options.PInitialWealth, Alpha = 10 * 0.000001 };
+            //options.DUtilityCalculator = new RiskNeutralUtilityCalculator() { InitialWealth = options.DInitialWealth };
+            return options;
+        }
+
+        public static MyGameOptions SingleRound_Usual()
+        {
+            var options = new MyGameOptions()
+            {
+                PInitialWealth = 1000000,
+                DInitialWealth = 1000000,
+                DamagesToAllege = 100000,
+                NumLitigationQualityPoints = 6,
+                NumSignals = 6,
+                NumOffers = 6,
                 MyGameDisputeGenerator = new MyGameEqualQualityProbabilitiesDisputeGenerator()
                 {
                     ProbabilityTrulyLiable_LitigationQuality75 = 0.75,
@@ -126,6 +199,8 @@ namespace ACESim
             //options.DUtilityCalculator = new RiskNeutralUtilityCalculator() { InitialWealth = options.DInitialWealth };
             return options;
         }
+
+
 
         public static MyGameOptions SingleRound_Ambitious()
         {
