0a925292018dbecb5a6752befa07409a117a4529
Michael Abramowicz <mbabramo@yahoo.com>
2019-06-04 13:49:29 -0400

    NodeActionsHistory improvements

--- ACESimBase/GameSolvingSupport/AcceleratedBestResponsePrep.cs
+++ ACESimBase/GameSolvingSupport/AcceleratedBestResponsePrep.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ACESim;
 
 namespace ACESimBase.GameSolvingSupport
 {
-    class AcceleratedBestResponsePrep
+    public class AcceleratedBestResponsePrep : ITreeNodeProcessor<NodeActionsHistory, NodeActionsHistory>
     {
+
+
+        public NodeActionsHistory InformationSet_Forward(InformationSetNode informationSet, IGameState predecessor, byte predecessorAction, NodeActionsHistory fromPredecessor)
+        {
+            NodeActions
+        }
+
+        public NodeActionsHistory InformationSet_Backward(InformationSetNode informationSet, IEnumerable<NodeActionsHistory> fromSuccessors)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NodeActionsHistory ChanceNode_Backward(ChanceNode chanceNode, IEnumerable<NodeActionsHistory> fromSuccessors, int distributorChanceInputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NodeActionsHistory ChanceNode_Forward(ChanceNode chanceNode, IGameState predecessor, byte predecessorAction, NodeActionsHistory fromPredecessor, int distributorChanceInputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NodeActionsHistory FinalUtilities_TurnAround(FinalUtilitiesNode finalUtilities, IGameState predecessor, byte predecessorAction, NodeActionsHistory fromPredecessor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ACESimBase/GameSolvingSupport/NodeActionsHistory.cs
+++ ACESimBase/GameSolvingSupport/NodeActionsHistory.cs
@@ -17,11 +17,11 @@ namespace ACESimBase.GameSolvingSupport
 
         }
 
-        public NodeActionsHistory DeepClone(int skip = 0)
+        public NodeActionsHistory DeepClone(int skip = 0, int take = int.MaxValue)
         {
             return new NodeActionsHistory()
             {
-                NodeActions = NodeActions.Select(x => x.Clone()).Skip(skip).ToList(),
+                NodeActions = NodeActions.Select(x => x.Clone()).Skip(skip).Take(take).ToList(),
                 SuccessorInformationSet = SuccessorInformationSet
             };
         }
@@ -37,7 +37,40 @@ namespace ACESimBase.GameSolvingSupport
             return HashCode.Combine(NodeActions, SuccessorInformationSet);
         }
 
+        public NodeActionsHistory WithAppended(IGameState node, byte actionAtNode, int distributorChanceInputs = -1)
+        {
+            var copied = NodeActions.ToList();
+            copied.Add(new NodeAction(node, actionAtNode, distributorChanceInputs));
+            return new NodeActionsHistory()
+            {
+                NodeActions = copied,
+                SuccessorInformationSet = null
+            };
+        }
+
+        public NodeActionsHistory WithFinalUtilitiesAppended(FinalUtilitiesNode node)
+        {
+
+            var copied = NodeActions.ToList();
+            return new NodeActionsHistory()
+            {
+                NodeActions = copied,
+                SuccessorInformationSet = node
+            };
+        }
+
+        /// <summary>
+        /// Returns another NodeActionsHistory object including all history since the last information set by the player, or all history if the player has not had an information set yet.
+        /// </summary>
+        /// <param name="nonChancePlayerIndex"></param>
+        /// <returns></returns>
         public NodeActionsHistory GetIncrementalHistory(byte nonChancePlayerIndex)
+        {
+            int? indexOfLastInformationSetByPlayer = GetIndexOfLastInformationSetByPlayer(nonChancePlayerIndex);
+            return DeepClone(indexOfLastInformationSetByPlayer == null ? 0 : (int)indexOfLastInformationSetByPlayer + 1);
+        }
+
+        private int? GetIndexOfLastInformationSetByPlayer(byte nonChancePlayerIndex)
         {
             int? indexOfLastInformationSetByPlayer = null;
             for (int i = NodeActions.Count - 1; i >= 0; i--)
@@ -46,10 +79,44 @@ namespace ACESimBase.GameSolvingSupport
                     indexOfLastInformationSetByPlayer = i;
                     break;
                 }
-            return DeepClone(indexOfLastInformationSetByPlayer == null ? 0 : (int)indexOfLastInformationSetByPlayer + 1);
+
+            return indexOfLastInformationSetByPlayer;
         }
 
+        /// <summary>
+        /// Returns another NodeActionsHistory object with all actions after the specified decision.
+        /// </summary>
+        /// <param name="decisionIndex"></param>
+        /// <returns></returns>
         public NodeActionsHistory GetSubsequentHistory(int decisionIndex)
+        {
+            int? indexOfInformationSetForDecision = GetIndexOfInformationSetForDecision(decisionIndex);
+            return DeepClone((int)indexOfInformationSetForDecision + 1);
+        }
+
+        /// <summary>
+        /// Returns another NodeActionsHistory object with all actions after the specified decision but only to a successor information set (i.e., final utilities or another information set node for the same non-chance player), which will then be included as the successor information set.
+        /// </summary>
+        /// <param name="decisionIndex"></param>
+        /// <param name="nonChancePlayerIndex"></param>
+        /// <returns></returns>
+        public NodeActionsHistory GetSubsequentHistoryToSuccessor(int decisionIndex, byte nonChancePlayerIndex)
+        {
+            int? indexOfInformationSetForDecision = GetIndexOfInformationSetForDecision(decisionIndex);
+            int? indexOfSuccessor = GetIndexOfLastInformationSetByPlayer(nonChancePlayerIndex);
+            if (indexOfSuccessor == null || indexOfSuccessor <= indexOfInformationSetForDecision)
+            {
+                if (SuccessorInformationSet == null)
+                    throw new Exception("No successor found.");
+                return DeepClone((int)indexOfInformationSetForDecision + 1); // including successor information set
+            }
+            // we want to skip the information set at indexOfInformationSetForDecision as well as the information set at indexOfSuccessor.
+            var result = DeepClone((int)indexOfInformationSetForDecision + 1, (int) indexOfSuccessor - (int) indexOfInformationSetForDecision - 1);
+            result.SuccessorInformationSet = NodeActions[(int)indexOfSuccessor].Node;
+            return result;
+        }
+
+        private int? GetIndexOfInformationSetForDecision(int decisionIndex)
         {
             int? indexOfInformationSetForDecision = null;
             for (int i = NodeActions.Count - 1; i >= 0; i--)
@@ -60,9 +127,15 @@ namespace ACESimBase.GameSolvingSupport
                 }
             if (indexOfInformationSetForDecision == null)
                 throw new ArgumentException("Expected decision not found; can't get subsequent history.");
-            return DeepClone((int)indexOfInformationSetForDecision + 1);
+            return indexOfInformationSetForDecision;
         }
 
+        /// <summary>
+        /// Returns a list of actions, excluding a specified player as well as distributed chance decisions and distributor chance input decisions, if chance decisions are being distributed. The decisions included are thus all those that may differentiate the action taken at the current information set.
+        /// </summary>
+        /// <param name="excludePlayerIndex"></param>
+        /// <param name="distributingChanceDecisions"></param>
+        /// <returns></returns>
         public ByteList GetActionsList(byte? excludePlayerIndex, bool distributingChanceDecisions)
         {
             return new ByteList(NodeActions
