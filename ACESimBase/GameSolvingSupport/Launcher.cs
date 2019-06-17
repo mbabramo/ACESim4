ea98a5b1d0e2bc607abb88153edd5e556ecab48e
Michael Abramowicz <mbabramo@yahoo.com>
2019-06-17 15:16:39 -0400

    ambitious 100,000 iterations

--- ACESimBase/GameSolvingSupport/Launcher.cs
+++ ACESimBase/GameSolvingSupport/Launcher.cs
@@ -18,14 +18,14 @@ namespace ACESim
         public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.MultiplicativeWeightsVanilla;
 
         public const int ProbingIterations = 20_000_000;
-        public const int VanillaIterations = 50_000;
+        public const int VanillaIterations = 100_000;
         public const int VanillaReportEveryNIterations = VanillaIterations;
         public const int VanillaBestResponseEveryMIterations = 1000;
         public const int MiniReportEveryPIterations = EffectivelyNever; 
         public const int CorrelatedEquilibriumCalculationsEveryNIterations = EffectivelyNever; 
         public const int RecordPastValuesEveryNIterations = EffectivelyNever; // used for correlated equilibrium calculations
         public const bool UseRandomPathsForReporting = true; 
-        public const int SummaryTableRandomPathsIterations = 2_000;
+        public const int SummaryTableRandomPathsIterations = 10_000;
 
         public const bool UseRegretAndStrategyDiscounting = true;
 
