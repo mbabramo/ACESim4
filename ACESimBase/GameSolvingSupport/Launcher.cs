320ccd7ac1e15213c42f883a0c11610dd7bcd380
Michael Abramowicz <mbabramo@yahoo.com>
2019-06-21 19:07:38 -0400

    more settings sort of work

--- ACESimBase/GameSolvingSupport/Launcher.cs
+++ ACESimBase/GameSolvingSupport/Launcher.cs
@@ -19,7 +19,7 @@ namespace ACESim
         public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.RegretMatching;
 
         public const int ProbingIterations = 20_000_000;
-        public const int VanillaIterations = 10_000; 
+        public const int VanillaIterations = 100_000; 
         public const int VanillaReportEveryNIterations = VanillaIterations;
         public const int VanillaBestResponseEveryMIterations = 1000; 
         public const int MiniReportEveryPIterations = EffectivelyNever; 
