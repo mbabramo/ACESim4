2925aee681fc7c920eaeffb3ff6e48cd49b9919c
Michael Abramowicz <mbabramo@yahoo.com>
2019-06-30 10:24:39 -0400

    more tests

--- ACESimBase/GameSolvingSupport/Launcher.cs
+++ ACESimBase/GameSolvingSupport/Launcher.cs
@@ -16,16 +16,16 @@ namespace ACESim
 
         #region Settings
 
-        public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.FictitiousSelfPlay;
+        public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.RegretMatching;
 
-        public const int VanillaIterations = 2_000;
+        public const int VanillaIterations = 20_000;
         public const int VanillaReportEveryNIterations = VanillaIterations;
-        public const int VanillaBestResponseEveryMIterations = 100; // DEBUG
+        public const int VanillaBestResponseEveryMIterations = 1000; // DEBUG
         public const int MiniReportEveryPIterations = EffectivelyNever; 
         public const int CorrelatedEquilibriumCalculationsEveryNIterations = EffectivelyNever; 
         public const int RecordPastValuesEveryNIterations = EffectivelyNever; // used for correlated equilibrium calculations
-        public const bool UseRandomPathsForReporting = true;
-        public const int SummaryTableRandomPathsIterations = 2_000;
+        public const bool UseRandomPathsForReporting = true; 
+        public const int SummaryTableRandomPathsIterations = 20_000;
         public const int ProbingIterations = 20_000_000;
 
         public const bool UseRegretAndStrategyDiscounting = false;
