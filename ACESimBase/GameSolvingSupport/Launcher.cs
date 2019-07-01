c74bf600579d7a133ed81ba241800992faac77e7
Michael Abramowicz <mbabramo@yahoo.com>
2019-07-01 11:33:58 -0400

    Revert "higher trial costs -> settlement"
    This reverts commit acfa7059536274f3b1aac89a1552d3ca9233a269.

--- ACESimBase/GameSolvingSupport/Launcher.cs
+++ ACESimBase/GameSolvingSupport/Launcher.cs
@@ -19,7 +19,7 @@ namespace ACESim
 
         public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.FictitiousSelfPlay; // DEBUG
 
-        public const int VanillaIterations = 2_000;
+        public const int VanillaIterations = 10_000;
         public const int VanillaReportEveryNIterations = VanillaIterations;
         public const int VanillaBestResponseEveryMIterations = 1000; // DEBUG
         public const int MiniReportEveryPIterations = EffectivelyNever; 
