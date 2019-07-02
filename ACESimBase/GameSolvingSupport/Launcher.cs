e5f6204f7b1dd09557676426ac5f5defed837b01
Michael Abramowicz <mbabramo@yahoo.com>
2019-07-01 21:11:03 -0400

    trying with fictitious self play

--- ACESimBase/GameSolvingSupport/Launcher.cs
+++ ACESimBase/GameSolvingSupport/Launcher.cs
@@ -17,7 +17,7 @@ namespace ACESim
 
         #region Settings
 
-        public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.RegretMatching; // DEBUG
+        public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.FictitiousSelfPlay; // DEBUG
 
         public const int VanillaIterations = 1_000;
         public const int VanillaReportEveryNIterations = VanillaIterations;
