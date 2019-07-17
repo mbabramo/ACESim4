a366c00b0c9bb43a435ab4643c1394a0673626a0
Michael Abramowicz <mbabramo@yahoo.com>
2019-07-17 14:14:05 -0400

    tried using RoleEnvironment -- but requires .Net framework

---
+++ ACESimBase/Util/AzureUtil.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACESimBase.Util
+{
+    public static class AzureUtil
+    {
+        private static bool m_IsRunningAzure = GetIsRunningInAzure();
+
+        private static bool GetIsRunningInAzure()
+        {
+            Guid guidId;
+            if (RoleEnvironment.IsAvailable && Guid.TryParse(RoleEnvironment.DeploymentId, out guidId))
+                return true;
+            return false;
+        }
+
+        public static bool IsRunningInAzure()
+        {
+            return m_IsRunningAzure;
+        }
+
+        private static bool m_IsRunningAzureOrDevFabric = GetIsRunningInAzureOrDevFabric();
+
+        private static bool GetIsRunningInAzureOrDevFabric()
+        {
+            return RoleEnvironment.IsAvailable;
+        }
+
+        public static bool IsRunningInAzureOrDevFabric()
+        {
+            return m_IsRunningAzureOrDevFabric;
+        }
+    }
+}
--- ACESimConsole/Program.cs
+++ ACESimConsole/Program.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Diagnostics;
 using ACESim.Util;
+using ACESimBase.Util;
 
 namespace ACESim
 {
@@ -81,21 +82,24 @@ namespace ACESim
                     break;
             }
             ReportCollection launchResult = await launcher.Launch();
-            TextCopy.Clipboard.SetText(launchResult.standardReport);
-            s.Stop();
-            TabbedText.WriteLineEvenIfDisabled($"Total runtime {s.Elapsed} ");
-            TabbedText.WriteLineEvenIfDisabled("");
-            TabbedText.WriteLineEvenIfDisabled("Press Enter to end (copying standard report to clipboard).");
-            TabbedText.WriteLineEvenIfDisabled("Press c to end (copying comma-separated report to clipboard).");
-            ConsoleKey key = Console.ReadKey(true).Key;
-            do
+            if (AzureUtil.IsRunningInAzureOrDevFabric() == false)
             {
-                while (!Console.KeyAvailable)
+                TextCopy.Clipboard.SetText(launchResult.standardReport);
+                s.Stop();
+                TabbedText.WriteLineEvenIfDisabled($"Total runtime {s.Elapsed} ");
+                TabbedText.WriteLineEvenIfDisabled("");
+                TabbedText.WriteLineEvenIfDisabled("Press Enter to end (copying standard report to clipboard).");
+                TabbedText.WriteLineEvenIfDisabled("Press c to end (copying comma-separated report to clipboard).");
+                ConsoleKey key = Console.ReadKey(true).Key;
+                do
                 {
-                    // Do something
-                }
-            } while (key != ConsoleKey.Enter && key != ConsoleKey.C);
-            TextCopy.Clipboard.SetText((key == ConsoleKey.C) ? String.Join("\r", launchResult.csvReports.SingleOrDefault() ?? "") : launchResult.standardReport);
+                    while (!Console.KeyAvailable)
+                    {
+                        // Do something
+                    }
+                } while (key != ConsoleKey.Enter && key != ConsoleKey.C);
+                TextCopy.Clipboard.SetText((key == ConsoleKey.C) ? String.Join("\r", launchResult.csvReports.SingleOrDefault() ?? "") : launchResult.standardReport);
+            }
         }
     }
 }
