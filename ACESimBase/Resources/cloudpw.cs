3a18c1d35304fe2bfc088bf177077b7926f722f0
Michael Abramowicz <mbabramo@yahoo.com>
2019-10-01 09:13:41 -0400

    add cloudpw

---
+++ ACESimBase/Resources/cloudpw.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACESimBase.Resources
+{
+    public static class cloudpw
+    {
+        public static string GetCloudStorageAccountConnectionString() => "DefaultEndpointsProtocol=https;AccountName=acesim;AccountKey=MoK8u3L4jcqS4x9jmRNRgi4glIu7B+bPTIH0kaLQ25HuOu2+XYP+XL5IDlkZYTlfcPxyjeueg5Fa5MLQRQ18vg==;EndpointSuffix=core.windows.net";
+    }
+}
