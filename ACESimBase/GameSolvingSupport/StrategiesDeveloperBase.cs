d139db4201edbdf7f40f66727d04b61b0013f240
mbabramo <mbabramo@yahoo.com>
2019-10-04 16:58:15 -0400

    debugging code

--- ACESimBase/GameSolvingSupport/StrategiesDeveloperBase.cs
+++ ACESimBase/GameSolvingSupport/StrategiesDeveloperBase.cs
@@ -344,9 +344,16 @@ namespace ACESim
             HistoryNavigationInfo navigationSettings = navigation ?? Navigation;
             return historyPoint.GetGameStateForCurrentPlayer(navigationSettings) ?? GetGameStateByPlayingUnderlyingGame(ref historyPoint, navigationSettings);
         }
-
+        static int DEBUG = 0;
         private unsafe IGameState GetGameStateByPlayingUnderlyingGame(ref HistoryPoint historyPoint, HistoryNavigationInfo navigationSettings)
         {
+            DEBUG++;
+            if (DEBUG == 6)
+            {
+                var DEBUG2 = 0;
+                GameProgressLogger.LoggingOn = true;
+                GameProgressLogger.DetailedLogging = true;
+            }
             IGameState gameState;
             List<byte> actionsSoFar = historyPoint.GetActionsToHere(navigationSettings);
             (GameProgress progress, _) = GamePlayer.PlayPath(actionsSoFar, false);
