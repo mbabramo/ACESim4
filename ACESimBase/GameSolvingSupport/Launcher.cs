0fcae322ef3b46c7e741caceffff471c87191f0c
mbabramo <mbabramo@yahoo.com>
2019-10-04 15:14:46 -0400

    changing launcher

--- ACESimBase/GameSolvingSupport/Launcher.cs
+++ ACESimBase/GameSolvingSupport/Launcher.cs
@@ -22,9 +22,9 @@ namespace ACESim
 
         public GameApproximationAlgorithm Algorithm = GameApproximationAlgorithm.RegretMatching;
 
-        public const int VanillaIterations = 1_000; // DEBUG
+        public const int VanillaIterations = 100_000; // DEBUG
         public const int VanillaReportEveryNIterations = VanillaIterations;
-        public const int VanillaBestResponseEveryMIterations = 1000; // DEBUG
+        public const int VanillaBestResponseEveryMIterations = 5000; // DEBUG
         public const bool CalculatePerturbedBestResponseRefinement = true;
         public const int MiniReportEveryPIterations = EffectivelyNever;
         public const bool AlwaysSuppressDisplayReportOnScreen = true;
