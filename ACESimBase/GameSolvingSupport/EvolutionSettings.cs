7505272515e27acea3b74ba2607b6e437798de91
Michael Abramowicz <mbabramo@yahoo.com>
2019-11-06 09:13:59 -0500

    revert settings

--- ACESimBase/GameSolvingSupport/EvolutionSettings.cs
+++ ACESimBase/GameSolvingSupport/EvolutionSettings.cs
@@ -76,7 +76,7 @@ namespace ACESim
         public static bool PredeterminePrunabilityBasedOnRelativeContributions = false; // if True, then we prune if and only if the action contributes negligibly at any later information set
 
         public bool CFRBR = false; // if true, opponent plays best response
-        public bool CFR_OpponentPlaysAverageStrategy = false; // typically, the opponent plays the current strategy in CFR. With this variant, the opponent plays the average strategy.
+        public bool CFR_OpponentPlaysAverageStrategy = false; // typically, the opponent plays the current strategy in CFR. With this variant, the opponent plays the average strategy. This sometimes gives mildly better results, but not consistent performance improvements.
 
         // For Vanilla algorithm:
         // From Solving Imperfect Information Games with Discounted Regret Minimization -- optimal values (for situations in which pruning may be used)
