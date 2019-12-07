03781fc933ce3f21d1fcf6dfa6880be54436a7c3
Michael Abramowicz <mbabramo@yahoo.com>
2019-12-07 17:04:54 -0500

    LitigCharts project

---
+++ LitigCharts/Program.cs
@@ -0,0 +1,25 @@
+﻿using ACESimBase.Util;
+using System;
+
+namespace LitigCharts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"H:\My Drive\Articles, books in progress\Machine learning model of litigation\bluffing results";
+
+            //todo; // check the proportion that settle in round 1 etc. Then, if proportion is sufficiently low for next round, leave it out altogether. (relevant for hicost).
+
+            //todo; // consider adding settlement percentages in Round header -- but then we'll need to repeat the header) -- we might have a big header "Round (Settlement %)" and then entries like 1 (76%).
+
+            string[] filenames = new[] { "R079 baseline", "R079 cfrplus", "R079 locost", "R079 hicost", "R079 pra", "R079 dra", "R079 bra", "R079 goodinf", "R079 badinf", "R079 pgdbinf", "R079 pbdginf" };
+            foreach (string filename in filenames)
+                InformationSetCharts.PlotPAndD_WithHidden(path, filename, numRounds: 3, numSignals: 5, numOffers: 5);
+
+            filenames = new[] { "R079 twobr" };
+            foreach (string filename in filenames)
+                InformationSetCharts.PlotPAndD_WithHidden(path, filename, numRounds: 2, numSignals: 5, numOffers: 5);
+        }
+    }
+}
