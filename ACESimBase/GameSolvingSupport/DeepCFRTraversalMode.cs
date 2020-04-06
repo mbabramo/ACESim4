09b0885ccd27a0f5d11ad1094fd2e50b77066f55
Michael Abramowicz <mbabramo@yahoo.com>
2020-04-06 13:30:28 -0400

    Revert "Revert "working on reporting""
    This reverts commit 1f02d928aad02b41af126a95635bb8ff585026ea.

---
+++ ACESimBase/GameSolvingSupport/DeepCFRTraversalMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACESimBase
+{
+    public enum DeepCFRTraversalMode
+    {
+        ProbeForUtilities,
+        AddRegretObservations,
+        PlaybackSinglePath
+    }
+}
