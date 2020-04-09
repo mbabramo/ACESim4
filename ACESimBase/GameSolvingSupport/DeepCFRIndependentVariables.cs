cef396f26b312d2e059c5ab89f74424c053cd3b1
mbabramo <mbabramo@yahoo.com>
2020-04-09 09:46:20 -0400

    treat action like items in info set

--- ACESimBase/GameSolvingSupport/DeepCFRIndependentVariables.cs
+++ ACESimBase/GameSolvingSupport/DeepCFRIndependentVariables.cs
@@ -15,14 +15,14 @@ namespace ACESimBase.GameSolvingSupport
         /// </summary>
         public byte DecisionIndex;
         /// <summary>
+        /// The action chosen by the player at the information set.
+        /// </summary>
+        public byte ActionChosen;
+        /// <summary>
         /// The information set will consist of actions of players that the player knows about, including but not limited to all of the player's own actions. Each of these will be an independent variable.
         /// If some decision indices exist for some players and not others, then there will also be an independent variable indicating whether the information is available.
         /// </summary>
         public List<(byte decisionIndex, byte information)> InformationSet;
-        /// <summary>
-        /// The action chosen by the player at the information set.
-        /// </summary>
-        public byte ActionChosen;
         /// <summary>
         /// If we change game parameters in each observation, then the game parameters for this observation can be specified here. If different game parameters are used for early iterations than for late iterations, then both sets can be included here. Game parameters may also include some indication of the extent to which each player takes other players' utilities into account.
         /// </summary>
@@ -44,16 +44,23 @@ namespace ACESimBase.GameSolvingSupport
             GameParameters = gameParameters;
         }
 
+        public IEnumerable<(byte decisionIndex, byte information)> InformationSetPlusActionChosen()
+        {
+            foreach (var item in InformationSet)
+                yield return item;
+            yield return (DecisionIndex, ActionChosen);
+        }
+
         public static List<(byte decisionIndex, bool includedForAll)> GetIncludedDecisionIndices(IEnumerable<DeepCFRIndependentVariables> independentVariablesSets)
         {
             HashSet<byte> includedDecisionIndices = new HashSet<byte>();
             foreach (var independentVariables in independentVariablesSets)
-                foreach (var decisionIndex in independentVariables.InformationSet.Select(x => x.decisionIndex))
+                foreach (var decisionIndex in independentVariables.InformationSetPlusActionChosen().Select(x => x.decisionIndex))
                     includedDecisionIndices.Add(decisionIndex);
             List<(byte decisionIndex, bool includedForAll)> result = 
                 includedDecisionIndices
                 .Select(decisionIndex => (decisionIndex, independentVariablesSets.All(
-                    iv => iv.InformationSet.Any(
+                    iv => iv.InformationSetPlusActionChosen().Any(
                         item => item.decisionIndex == decisionIndex)
                     )
                 )).ToList();
@@ -62,32 +69,33 @@ namespace ACESimBase.GameSolvingSupport
 
         public float[] AsArray(bool includePlayer, bool includeDecision, List<(byte decisionIndex, bool includedForAll)> includedDecisionIndices)
         {
-            int informationSetSize = InformationSet?.Count() ?? 0;
+            int informationSetSize = InformationSetPlusActionChosen()?.Count() ?? 0;
             int numGameParameters = GameParameters?.Count() ?? 0;
-            int arraySize = (includePlayer ? 1 : 0) + (includeDecision ? 1 : 0) + 1 /* for action */ + includedDecisionIndices.Count() + includedDecisionIndices.Where(x => x.includedForAll == false).Count() + numGameParameters;
+            int arraySize = (includePlayer ? 1 : 0) + (includeDecision ? 1 : 0) + includedDecisionIndices.Count() + includedDecisionIndices.Where(x => x.includedForAll == false).Count() + numGameParameters;
             float[] result = new float[arraySize];
             int resultIndex = 0;
             if (includePlayer)
                 result[resultIndex++] = Player;
             if (includeDecision)
                 result[resultIndex++] = DecisionIndex;
-            result[resultIndex++] = ActionChosen;
             int informationSetIndex = 0;
             foreach ((byte decisionIndex, bool includedForAll) in includedDecisionIndices)
             {
                 int? nextInformationSetDecisionIndex = informationSetIndex >= InformationSet.Count() ? null : (int?) InformationSet[informationSetIndex].decisionIndex;
                 if (nextInformationSetDecisionIndex == null || nextInformationSetDecisionIndex > decisionIndex)
-                { // the decisionIndex is not included in the information set. Note this if necessary.
+                { 
+                    // the decisionIndex is not included in the information set.
                     if (!includedForAll)
                     {
-                        result[resultIndex++] = 0;
+                        result[resultIndex++] = 0; // i.e., this information is unavailable
                     }
+                    result[resultIndex++] = 0; // this is where information would have gone if available
                 }
                 else
                 {// the decisionIndex is included in the information set. Note this if necessary. Then, record the information itself in the result.
                     if (!includedForAll)
                     {
-                        result[resultIndex++] = 1.0f;
+                        result[resultIndex++] = 1.0f; // i.e., information is available
                     }
                     if (resultIndex == result.Length)
                         throw new Exception("DEBUG");
