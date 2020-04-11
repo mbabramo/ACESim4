125153af0a96993d200be2842456ae53975cb9a3
mbabramo <mbabramo@yahoo.com>
2020-04-11 14:31:30 -0400

    draft MLNetRegression

--- ACESimBase/Util/MLNetRegression.cs
+++ ACESimBase/Util/MLNetRegression.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.ML;
 using Microsoft.ML.AutoML;
 using ACESim;
 using System.Linq;
@@ -13,99 +12,89 @@ namespace ACESimBase.Util
 {
     public class MLNetRegression : IRegression
     {
-        Type dynamicType;
         SchemaDefinition Schema;
+        MLContext Context;
+        ITransformer Transformer; 
+        PredictionEngine<MLNetDatum, MLNetPrediction> PredictionEngine;
 
-        public class MyData
+        public class MLNetDatum
         {
-            public float[] Dependent { get; set; }
-            public float[] Independent { get; set; }
+            /// <summary>
+            /// The dependent variables (Y)
+            /// </summary>
+            public float[] DependentVariables { get; set; }
+            /// <summary>
+            /// The independent variables (X)
+            /// </summary>
+            public float[] Features { get; set; }
         }
 
-        public void InitializeSchemaDefinition((float[] X, float[] Y)[] data)
+        public class MLNetPrediction
         {
-            var exampleDatum = data.First();
-            Schema = SchemaDefinition.Create(typeof(MyData));
-            Schema["Dependent"].ColumnType = new VectorDataViewType(NumberDataViewType.Single, exampleDatum.Y.Length);
-            Schema["Independent"].ColumnType = new VectorDataViewType(NumberDataViewType.Single, exampleDatum.X.Length);
+            public float[] DependentVariables { get; set; }
+            // Score produced from the trainer.
+            public float Score { get; set; }
         }
+
+        public void InitializeSchemaDefinitionIfNecessary(float[] X, float[] Y)
+        {
+            if (Schema != null)
+                return;
+            Schema = SchemaDefinition.Create(typeof(MLNetDatum));
+            Schema[nameof(MLNetDatum.DependentVariables)].ColumnType = new VectorDataViewType(NumberDataViewType.Single, Y.Length);
+            Schema[nameof(MLNetDatum.Features)].ColumnType = new VectorDataViewType(NumberDataViewType.Single, X.Length);
+        }
+
+        public IDataView DatumToDataView(float[] X, float[] Y = null) => ArrayToDataView(Context, new (float[] X, float[] Y)[] { (X, Y) });
 
         public IDataView ArrayToDataView(MLContext mlContext, (float[] X, float[] Y)[] data)
         {
-            var myData = data.Select(d => new MyData() { Dependent = d.Y, Independent = d.X }).ToArray();
-            mlContext.Data.LoadFromEnumerable(myData, Schema);
-        }
-
-        public IDataView ArrayToDataView2((float[] X, float[] Y)[] data)
-        {
-            InitializeSchemaDefinitionIfNecessary(data);
-
-            // create dynamic list
-            IEnumerable<object> dynamicList = DynamicType.CreateDynamicList(dynamicType);
-
-            // get an action that will add to the list
-            var addAction = DynamicType.GetAddAction(dynamicList);
-
-            foreach (var datum in data)
-            {
-                object[] toAdd = new object[datum.Y.Length + datum.X.Length];
-                for (int i = 0; i < datum.Y.Length; i++)
-                    toAdd[i] = datum.Y[i];
-                for (int i = 0; i < datum.X.Length; i++)
-                    toAdd[datum.Y.Length + i] = datum.X[i];
-                addAction.Invoke(toAdd);
-            }
-
-            var mlContext = new MLContext();
-            var dataType = mlContext.Data.GetType();
-            var loadMethodGeneric = dataType.GetMethods().First(method => method.Name == "LoadFromEnumerable" && method.IsGenericMethod);
-            var loadMethod = loadMethodGeneric.MakeGenericMethod(dynamicType);
-            var dataView = (IDataView)loadMethod.Invoke(mlContext.Data, new[] { dynamicList, Schema });
-
-            return dataView;
-        }
-
-        private void InitializeSchemaDefinitionIfNecessary((float[] X, float[] Y)[] data)
-        {
-            if (dynamicType != null)
-                return;
-            List<DynamicTypeProperty> properties = new List<DynamicTypeProperty>();
-            for (int y = 0; y < data.First().Y.Length; y++)
-                properties.Add(new DynamicTypeProperty($"Y{y}", typeof(float)));
-            for (int x = 0; x < data.First().X.Length; x++)
-                properties.Add(new DynamicTypeProperty($"X{x}", typeof(float)));
-
-            dynamicType = DynamicType.CreateDynamicType(properties);
-            Schema = SchemaDefinition.Create(dynamicType);
+            var datum = data.First();
+            InitializeSchemaDefinitionIfNecessary(datum.X, datum.Y);
+            var myData = data.Select(d => new MLNetDatum() { DependentVariables = d.Y, Features = d.X }).ToArray();
+            return mlContext.Data.LoadFromEnumerable(myData, Schema);
         }
 
         public Task Regress((float[] X, float[] Y)[] data)
         {
-            MLContext mlContext = new MLContext();
-            mlContext.Data.CreateTextLoader();
-            IDataView trainDataView = ArrayToDataView(data);
+            Context = new MLContext();
+            IDataView trainDataView = ArrayToDataView(Context, data);
+            IEstimator<ITransformer> estimator = null;
+            estimator = Context.Regression.Trainers.FastForest(nameof(MLNetDatum.DependentVariables), nameof(MLNetDatum.Features));
+            // estimator = ChooseEstimatorExperimentally(mlContext, trainDataView);
+            Transformer = estimator.Fit(trainDataView);
+            PredictionEngine = Context.Model.CreatePredictionEngine<MLNetDatum, MLNetPrediction>(Transformer);
+            return Task.CompletedTask;
+        }
+
+        private IEstimator<ITransformer> ChooseEstimatorExperimentally(MLContext mlContext, IDataView trainDataView)
+        {
             var experimentSettings = new RegressionExperimentSettings();
             experimentSettings.MaxExperimentTimeInSeconds = 10;
             experimentSettings.OptimizingMetric = RegressionMetric.RSquared;
             //experimentSettings.Trainers.Clear();
             //experimentSettings.Trainers.Add(RegressionTrainer.FastTree);
-            RegressionExperiment experiment = mlContext.Auto().CreateRegressionExperiment(experimentSettings); 
-            ExperimentResult<RegressionMetrics> experimentResult = experiment.Execute(trainDataView); 
+            RegressionExperiment experiment = mlContext.Auto().CreateRegressionExperiment(experimentSettings);
+            ExperimentResult<RegressionMetrics> experimentResult = experiment.Execute(trainDataView);
             RegressionMetrics metrics = experimentResult.BestRun.ValidationMetrics;
-            var model = experimentResult.BestRun.Model;
-            var DEBUG = experimentResult.BestRun.Estimator;
-            model.Transform()
-            model.Transform()
+            Transformer = experimentResult.BestRun.Model;
+            IEstimator<ITransformer> estimator = experimentResult.BestRun.Estimator;
+            return estimator;
         }
 
         public float[] GetResults(float[] x)
         {
-            throw new NotImplementedException();
+            MLNetPrediction prediction = new MLNetPrediction();
+            PredictionEngine.Predict(new MLNetDatum() { Features = x }, ref prediction);
+            return prediction.DependentVariables;
+            //IDataView transformed = Transformer.Transform(DatumToDataView(x));
+            //var result = Context.Data.CreateEnumerable<MLNetPrediction>(transformed, reuseRowObject: false).First();
+            //return result.DependentVariables;
         }
 
         public string GetTrainingResultString()
         {
-            throw new NotImplementedException();
+            return "";
         }
     }
 }
