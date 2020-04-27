be68573fe58b07992f3feab7dbef057641e641dc
Michael Abramowicz <mbabramo@yahoo.com>
2020-04-27 14:37:18 -0400

    GameProgressTree extensions in progress

--- ACESimBase/GameSolvingSupport/GameProgressTree.cs
+++ ACESimBase/GameSolvingSupport/GameProgressTree.cs
@@ -10,27 +10,88 @@ namespace ACESimBase.GameSolvingSupport
 {
     public class GameProgressTree : IEnumerable<GameProgress> 
     {
+        // DEBUG -- initially, we allocate based on action probabilities. These might be epsilon-probabilities. Then we allocate based on the child cumulative proportions. So, we can generalize the idea of action probabilities to represent child cumulative proportions. And then we should be able to get rid of CumulativeProportion. 
+
+        public class GameProgressTreeAllocationInfo
+        {
+            /// <summary>
+            /// The index in the list of allocations for the tree.
+            /// </summary>
+            public int AllocationIndex;
+            public int? MinDecisionIndexReached;
+        }
+
+        /// <summary>
+        /// Information on a node in the GameProgressTree, pursuant to particular criteria for allocating observations.  Observations may be allocated (1) pursuant to particular exploration probabilities for each player; OR (2) pursuant to specified allocation proportions. After we initially explore using a set of exploration probabilities (which may be zero, if the players are playing on-policy), we then calculate the next criterion by aggregating up the tree the cumulative reach probability pursuant to the original criterion of all game progresses that reach the first decision not reached in all game paths.
+        /// </summary>
+        public class GameProgressTreeNodeAllocation
+        {
+            public (int, int) ObservationRange;
+            public int NumObservations => ObservationRange.Item2 - ObservationRange.Item1 + 1;
+            /// <summary>
+            /// Where encompassing all game progresses (regardless of the decision being played up to), the action probabilities at this game point; these will be set by the algorithm allocating observations. When encompassing only game progresses that reach at least a particular decision index, before the allocation algorithm begins, these are set to reflect the cumulative reach probability of game progresses lower in the tree reach the necessary point, and then when the algorithm elaborates the tree, this will reflect the action probabilities.
+            /// </summary>
+            public double[] ChildProportions;
+            /// <summary>
+            /// The cumulative reach probability of this node for the allocation. 
+            /// </summary>
+            public double CumulativeReachProbability;
+        }
+
+        /// <summary>
+        /// Information on the probability of playing to and from this node, given the exploration scheme in force.
+        /// </summary>
+        public class GameProgressTreeNodeProbabilities
+        {
+            /// <summary>
+            /// The probability that each player will play to this node given the exploration probabilities for each player. Note that this does not reflect the child proportions that are used.
+            /// </summary>
+            public double[] PlayToHereProbabilities;
+            /// <summary>
+            /// The probability that all players combined will play to this node.
+            /// </summary>
+            public double CombinedProbability;
+            /// <summary>
+            /// The probability that the player at this node will choose each of the child actions, taking into account the players' exploration values.
+            /// </summary>
+            public double[] ActionProbabilities;
+            /// <summary>
+            /// The allocations of observations pursuant to these probabilities. Each allocation must be narrower than the previous, so once we have an allocation that has null for ChildProportions, there will be no further allocations.
+            /// </summary>
+            public List<GameProgressTreeNodeAllocation> Allocations = new List<GameProgressTreeNodeAllocation>();
+
+            public void CreateNextAllocation(int allocationIndexToCreate, List<double> childReachProbabilities)
+            {
+                if (!Allocations.Any())
+                    throw new Exception();
+                int previousAllocationIndex = allocationIndexToCreate - 1;
+                GameProgressTreeNodeAllocation previousAllocation = Allocations[previousAllocationIndex];
+
+                double sum = childReachProbabilities.Sum();
+                if (sum == 0)
+                    return; // nothing more to allocate. 
+                double[] childProportions = childReachProbabilities.Select(x => x / sum).ToArray();
+
+                GameProgressTreeNodeAllocation newAllocation = new GameProgressTreeNodeAllocation()
+                {
+                    ChildProportions = childProportions,
+                    CumulativeReachProbability = sum
+                };
+            }
+        }
+
         public class GameProgressTreeNodeInfo
         {
             public IDirectGamePlayer DirectGamePlayer;
             public GameProgress GameProgress => DirectGamePlayer.GameProgress;
-            public (int, int) ObservationRange;
-            public int NumObservations => ObservationRange.Item2 - ObservationRange.Item1 + 1;
-            public double CumulativeProportion; // represents the proportion of observations represented by the node
-            public double ChildObservationsToChildCumulativeProportion(int numChildObservations) => CumulativeProportion * ((double)numChildObservations) / ((double)NumObservations);
-            public double[] ActionProbabilities;
-            public bool IsLeaf => ActionProbabilities == null;
 
+            public GameProgressTreeNodeAllocation Allocation = new GameProgressTreeNodeAllocation();
 
             public override string ToString()
             {
-                string actionProbabilitiesString = ActionProbabilities == null ? null : String.Join(",", ActionProbabilities.Select(x => x.ToSignificantFigures(3)));
+                string childProportionsString = ChildProportions == null ? null : String.Join(",", ChildProportions.Select(x => x.ToSignificantFigures(3)));
                 string resultString = GameProgress.GameComplete ? String.Join(",", GameProgress.GetNonChancePlayerUtilities().Select(x => x.ToSignificantFigures(5))) : "incomplete";
-                if (CumulativeProportion < 0.0001)
-                {
-                    var DEBUG = 0;
-                }
-                string result = $"Obs: {ObservationRange} Cumulative proportion: {CumulativeProportion.ToSignificantFigures_WithSciNotationForVerySmall(4)} Decision: {DirectGamePlayer.CurrentDecision?.Name} Probs: {actionProbabilitiesString} Result: {resultString}";
+                string result = $"Obs: {ObservationRange} Cumulative proportion: {PlayToHereProbabilities.ToSignificantFigures_WithSciNotationForVerySmall(4)} Decision: {DirectGamePlayer.CurrentDecision?.Name} Probs: {childProportionsString} Result: {resultString}";
                 return result;
             }
         }
@@ -46,7 +107,7 @@ namespace ACESimBase.GameSolvingSupport
                 {
                     DirectGamePlayer = directGamePlayer,
                     ObservationRange = (1, totalObservations),
-                    CumulativeProportion = 1.0,
+                    PlayToHereProbabilities = 1.0,
                 }
             };
             InitialRandSeed = randSeed;
@@ -60,13 +121,44 @@ namespace ACESimBase.GameSolvingSupport
             // DEBUG CompleteIncompletePlayers(doParallel);
         }
 
+        private void AccumulateCombined(bool doParallel)
+        {
+            Tree.WalkTree(treeStorage => { }, treeStorage =>
+            {
+                var node = treeStorage.StoredValue;
+                byte numPossibleActions = node.DirectGamePlayer.CurrentDecision.NumPossibleActions;
+                var results = Enumerable.Range(1, numPossibleActions).Select(a =>
+                {
+                    var branch = treeStorage.GetBranch((byte)a);
+                    if (branch == null)
+                        return 0;
+                    var branchNode = branch.StoredValue;
+                    double probability = branchNode.CombinedProbability;
+                    return probability;
+                }).Sum();
+                node.CombinedProbability = probability;
+            });
+        }
+
         public (byte branchID, GameProgressTreeNodeInfo childBranch, bool isLeaf)[] CreateSubbranches(GameProgressTreeNodeInfo sourceNode)
         {
-            double[] actionProbabilities = sourceNode.DirectGamePlayer.GetActionProbabilities();
-            sourceNode.ActionProbabilities = actionProbabilities;
+            if (sourceNode.ChildProportions == null)
+            {
+                // The child proportions have not been set. Thus, we use the action probabilities.
+                double[] actionProbabilities = sourceNode.DirectGamePlayer.GetActionProbabilities();
+                sourceNode.ChildProportions = actionProbabilities;
+            }
             int randSeed = sourceNode.DirectGamePlayer.GameProgress.GetActionsPlayedHash(InitialRandSeed);
-            (int, int)?[] subranges = DivideRangeIntoSubranges(sourceNode.ObservationRange, actionProbabilities, randSeed);
-            IDirectGamePlayer[] children = Enumerable.Range(1, actionProbabilities.Length).Select(a =>
+            (int, int)?[] subranges = DivideRangeIntoSubranges(sourceNode.ObservationRange, sourceNode.ChildProportions, randSeed);
+            if (sourceNode.ChildProportions == null)
+            {
+                double numObservations = ((double)sourceNode.NumObservations);
+                sourceNode.ChildProportions = subranges.Select(x =>
+                {
+                    return x == null ? 0 : ((double)x.Value.Item2 - x.Value.Item1 + 1) / numObservations;
+                }).ToArray();
+            }
+            IDirectGamePlayer[] children = Enumerable.Range(1, sourceNode.ChildProportions.Length).Select(a =>
             {
                 (int, int)? subrange = subranges[a - 1];
                 if (subrange == null)
@@ -85,7 +177,7 @@ namespace ACESimBase.GameSolvingSupport
                 return childGamePlayer;
             }).ToArray();
             List<(byte branchID, GameProgressTreeNodeInfo childBranch, bool isLeaf)> subbranches = new List<(byte branchID, GameProgressTreeNodeInfo childBranch, bool isLeaf)>();
-            for (byte a = 1; a <= actionProbabilities.Length; a++)
+            for (byte a = 1; a <= sourceNode.ChildProportions.Length; a++)
             {
                 int numInSubrange = subranges[a - 1] == null ? 0 : subranges[a - 1].Value.Item2 - subranges[a - 1].Value.Item1 + 1;
                 IDirectGamePlayer gamePlayer = children[a - 1];
@@ -93,7 +185,7 @@ namespace ACESimBase.GameSolvingSupport
                 {
                     (int, int) childObservationRange = subranges[a - 1].Value;
                     int childNumObservations = childObservationRange.Item2 - childObservationRange.Item1 + 1;
-                    var childBranchItem = new GameProgressTreeNodeInfo() { DirectGamePlayer = gamePlayer, ObservationRange = childObservationRange, CumulativeProportion = sourceNode.ChildObservationsToChildCumulativeProportion(childNumObservations) };
+                    var childBranchItem = new GameProgressTreeNodeInfo() { DirectGamePlayer = gamePlayer, ObservationRange = childObservationRange, PlayToHereProbabilities = sourceNode.ChildObservationsToChildCumulativeProportion(childNumObservations) };
                     subbranches.Add((a, childBranchItem, gamePlayer.GameProgress.GameComplete)); // If there is only 1 in subrange, we go no further, BUT we will need to complete those games later. DEBUG -- no, we should go further, until the end; we go no further only where the game is complete
                 }
             }
