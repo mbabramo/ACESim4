d8ce7a9af93bf705c3f3d45ae57981b1eb5c7002
mbabramo <mbabramo@yahoo.com>
2021-01-25 16:39:16 -0500

    fixes

--- LitigCharts/FeeShiftingData.cs
+++ LitigCharts/FeeShiftingData.cs
@@ -17,17 +17,19 @@ namespace LitigCharts
             var map = launcher.GetFeeShiftingArticleNameMap(); // name to find (avoids redundancies)
             string[] filtersOfRowsToGet = new string[] { "All" };
             string[] columnsToGet = new string[] { "PFiles", "DAnswers" };
+            string filePrefix = "FS022-";
+            string fileSuffix = "Corr";
             foreach (var gameOptionsSet in gameOptionsSets)
             {
-                string filenameWithoutCSV = map[gameOptionsSet.Name];
-                string filename = filenameWithoutCSV + ".csv";
+                string filenameCore = map[gameOptionsSet.Name];
+                string filename = filePrefix + filenameCore + fileSuffix + ".csv";
                 string path = @"C:\Users\Admin\Documents\GitHub\ACESim4\ReportResults";
-                string combined = Path.Combine(path, filenameWithoutCSV);
+                string combined = Path.Combine(path, filename);
                 (string columnName, string expectedText)[][] rowsToFind = new (string columnName, string expectedText)[filtersOfRowsToGet.Length][];
                 for (int f = 0; f < filtersOfRowsToGet.Length; f++)
                 {
                     rowsToFind[f] = new (string columnName, string expectedText)[2];
-                    rowsToFind[f][0] = ("OptionSet", filenameWithoutCSV);
+                    rowsToFind[f][0] = ("OptionSet", filenameCore);
                     rowsToFind[f][1] = ("Filter", filtersOfRowsToGet[f]);
                 }
                 // string[] columnsToGet = new string[] { "Trial", "AccSq", "POffer", "DOffer" };
