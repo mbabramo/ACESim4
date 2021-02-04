4bdf4f6435ebadf66fddb68e662424a6fd439163
mbabramo <mbabramo@yahoo.com>
2021-02-04 15:13:46 -0500

    IPotentiallyExactValue

--- ACESimBase/GameSolvingSupport/ExactValue.cs
+++ ACESimBase/GameSolvingSupport/ExactValue.cs
@@ -8,120 +8,118 @@ using System.Threading.Tasks;
 
 namespace ACESimBase.GameSolvingSupport
 {
-    public readonly struct ExactValue
+    public readonly struct ExactValue : IPotentiallyExactValue<ExactValue>
     {
-		public static bool AbbreviateExactValues = false;
+        public static bool AbbreviateExactValues = false;
 
         private readonly Rational V;
 
-		public ExactValue(Rational v)
-		{
-			V = v;
-		}
+        public ExactValue(Rational v)
+        {
+            V = v;
+        }
 
-		public static ExactValue Zero() => ExactValue.FromInteger(0);
-		public static ExactValue One() => ExactValue.FromInteger(1);
-		public static ExactValue FromInteger(int i) => new ExactValue((Rational) i);
-		public static ExactValue FromRational(Rational r) => new ExactValue(r);
+        public static IPotentiallyExactValue<ExactValue> Zero() => ExactValue.FromInteger(0);
+        public static IPotentiallyExactValue<ExactValue> One() => ExactValue.FromInteger(1);
+        public static IPotentiallyExactValue<ExactValue> FromInteger(int i) => new ExactValue((Rational)i);
+        public static IPotentiallyExactValue<ExactValue> FromRational(Rational r) => new ExactValue(r);
 
-		public static implicit operator ExactValue(Rational b) => new ExactValue(b);
+        public bool IsPositive()
+        {
+            return V > 0;
+        }
+        public bool IsNegative()
+        {
+            return V < 0;
+        }
 
-		public bool IsPositive()
-		{
-			return V > 0;
-		}
-		public bool IsNegative()
-		{
-			return V < 0;
-		}
+        public bool IsZero()
+        {
+            return V.IsZero;
+        }
 
-		public bool IsZero()
-		{
-			return V.IsZero;
-		}
+        public bool IsOne()
+        {
+            return V.IsOne;
+        }
 
-		public bool IsOne()
-		{
-			return V.IsOne;
-		}
+        public IPotentiallyExactValue<ExactValue> CanonicalForm => FromRational(V.CanonicalForm);
 
-		public ExactValue CanonicalForm => V.CanonicalForm;
+        public IPotentiallyExactValue<ExactValue> Numerator => ExactValue.FromRational(V.Numerator);
+        public IPotentiallyExactValue<ExactValue> Denominator => ExactValue.FromRational(V.Denominator);
+        public double AsDouble => (double)V;
+        public Rational AsRational => V;
+        public bool IsExact => true;
 
-		public ExactValue Numerator => ExactValue.FromRational(V.Numerator);
-		public ExactValue Denominator => ExactValue.FromRational(V.Denominator);
-		public double AsDouble => (double)V;
-		public Rational AsRational => V;
-		public bool IsExact => true;
+        public bool IsEqualTo(IPotentiallyExactValue<ExactValue> b) => V == b.AsRational;
+        public bool IsNotEqualTo(IPotentiallyExactValue<ExactValue> b) => V != b.AsRational;
 
-		public bool IsEqualTo(ExactValue b) => V == b.V;
-		public bool IsNotEqualTo(ExactValue b) => V != b.V;
+        public override string ToString()
+        {
+            string s;
 
-		public override string ToString()
-		{
-			string s;
+            if (V.Denominator == 1)
+            {
+                s = V.ToString();
+                if (s.Length > 8 && AbbreviateExactValues)
+                    return V.ToString("E5");
+                else
+                    return s;
+            }
 
-			if (V.Denominator == 1)
-			{
-				s = V.ToString();
-				if (s.Length > 8 && AbbreviateExactValues)
-					return V.ToString("E5");
-				else
-					return s;
-			}
+            if (V.Denominator == 1)
+                s = V.Numerator.ToString();
+            else if (V.Denominator == 0)
+                s = $"{V.Numerator}/{V.Denominator}"; // show illegal div by 0
+            else
+                s = V.ToString();
+            return s;
+        }
 
-			if (V.Denominator == 1)
-				s = V.Numerator.ToString();
-			else if (V.Denominator == 0)
-				s = $"{V.Numerator}/{V.Denominator}"; // show illegal div by 0
-			else
-				s = V.ToString();
-			return s;
-		}
+        public IPotentiallyExactValue<ExactValue> LeastCommonMultiple(IPotentiallyExactValue<ExactValue> b)
+        /* a = least common multiple of a, b; b is preserved */
+        {
+            if (V.Denominator != 1 || b.AsRational.Denominator != 1)
+                throw new Exception("LeastCommonMultiple operation not available.");
+            var result = Times(b).DividedBy(IPotentiallyExactValue<ExactValue>.FromRational((Rational)BigInteger.GreatestCommonDivisor(V.Numerator, b.AsRational.Numerator)));
+            return result.CanonicalForm;
+        }
 
-		public ExactValue LeastCommonMultiple(ExactValue b)
-		/* a = least common multiple of a, b; b is preserved */
-		{
-			if (V.Denominator != 1 || b.V.Denominator != 1)
-				throw new Exception("LeastCommonMultiple operation not available.");
-			var result = Times(b).DividedBy((Rational) BigInteger.GreatestCommonDivisor(V.Numerator, b.V.Numerator));
-			return result.CanonicalForm;
-		}
+        public bool IsGreaterThan(IPotentiallyExactValue<ExactValue> b)
+        {
+            return V > b.AsRational;
+        }
 
-		public bool IsGreaterThan(ExactValue b)
-		{
-			return V > b.V;
-		}
+        public bool IsLessThan(IPotentiallyExactValue<ExactValue> b)
+        {
+            return V < b.AsRational;
+        }
 
-		public bool IsLessThan(ExactValue b)
-		{
-			return V < b.V;
-		}
+        public IPotentiallyExactValue<ExactValue> Plus(IPotentiallyExactValue<ExactValue> b)
+        {
+            return IPotentiallyExactValue<ExactValue>.FromRational((V + b.AsRational).CanonicalForm);
+        }
 
-		public ExactValue Plus(ExactValue b)
-		{
-			return (V + b.V).CanonicalForm;
-		}
+        public IPotentiallyExactValue<ExactValue> Minus(IPotentiallyExactValue<ExactValue> b)
+        {
+            return IPotentiallyExactValue<ExactValue>.FromRational((V - b.AsRational).CanonicalForm);
+        }
 
-		public ExactValue Minus(ExactValue b)
-		{
-			return (V - b.V).CanonicalForm;
-		}
+        public IPotentiallyExactValue<ExactValue> Negated() => Zero().Minus(this);
 
-		public ExactValue Negated() => Zero().Minus(this);
+        public IPotentiallyExactValue<ExactValue> Times(IPotentiallyExactValue<ExactValue> b)
+        {
+            return IPotentiallyExactValue<ExactValue>.FromRational((V * b.AsRational).CanonicalForm);
+        }
 
-		public ExactValue Times(ExactValue b)
-		{
-			return (V * b.V).CanonicalForm;
-		}
-
-		public ExactValue DividedBy(ExactValue b)
-		{
-			return (V / b.V).CanonicalForm;
-		}
+        public IPotentiallyExactValue<ExactValue> DividedBy(IPotentiallyExactValue<ExactValue> b)
+        {
+            return IPotentiallyExactValue<ExactValue>.FromRational((V / b.AsRational).CanonicalForm);
+        }
 
         public override int GetHashCode()
         {
             return V.GetHashCode();
         }
-	}
+    }
 }
---
+++ ACESimBase/GameSolvingSupport/IPotentiallyExactValue.cs
@@ -0,0 +1,34 @@
+﻿using Rationals;
+
+namespace ACESimBase.GameSolvingSupport
+{
+    public interface IPotentiallyExactValue<T> where T : IPotentiallyExactValue<T>
+    {
+        double AsDouble { get; }
+        Rational AsRational { get; }
+        IPotentiallyExactValue<T> CanonicalForm { get; }
+        IPotentiallyExactValue<T> Denominator { get; }
+        bool IsExact { get; }
+        IPotentiallyExactValue<T> Numerator { get; }
+
+        IPotentiallyExactValue<T> DividedBy(IPotentiallyExactValue<T> b);
+        int GetHashCode();
+        bool IsEqualTo(IPotentiallyExactValue<T> b);
+        bool IsGreaterThan(IPotentiallyExactValue<T> b);
+        bool IsLessThan(IPotentiallyExactValue<T> b);
+        bool IsNegative();
+        bool IsNotEqualTo(IPotentiallyExactValue<T> b);
+        bool IsOne();
+        bool IsPositive();
+        bool IsZero();
+        IPotentiallyExactValue<T> LeastCommonMultiple(IPotentiallyExactValue<T> b);
+        IPotentiallyExactValue<T> Minus(IPotentiallyExactValue<T> b);
+        IPotentiallyExactValue<T> Negated();
+        IPotentiallyExactValue<T> Plus(IPotentiallyExactValue<T> b);
+        IPotentiallyExactValue<T> Times(IPotentiallyExactValue<T> b);
+        public static IPotentiallyExactValue<T> Zero() => IPotentiallyExactValue<T>.FromInteger(0);
+        public static IPotentiallyExactValue<T> One() => IPotentiallyExactValue<T>.FromInteger(1);
+        public static IPotentiallyExactValue<T> FromInteger(int i) => throw new System.Exception();
+        public static IPotentiallyExactValue<T> FromRational(Rational r) => throw new System.Exception();
+    }
+}
