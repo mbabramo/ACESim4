67964af7941c4064f3cad5f124ecc5c100298774
mbabramo <mbabramo@yahoo.com>
2021-02-26 22:23:55 -0500

    prepare for FS031 (cutting off zero probabilities)

--- ACESimBase/GameSolvingSupport/EvolutionSettings.cs
+++ ACESimBase/GameSolvingSupport/EvolutionSettings.cs
@@ -66,7 +66,7 @@ namespace ACESim
         public bool CreateEquilibriaFileForSequenceForm = true;
         public bool PreloadedEquilibriaForSequenceForm = false; 
         public bool SkipIfEquilibriumFileAlreadyExists = false;
-        public bool SequenceFormCutOffProbabilityZeroNodes = false; // If true, then instead of setting chance nodes to no less than the lowest permissible rational value, probability is set to zero, and that part of the tree will be excised. (The algorithm will not work if probabilities are left at zero.)
+        public bool SequenceFormCutOffProbabilityZeroNodes = true; // If true, then instead of setting chance nodes to no less than the lowest permissible rational value, probability is set to zero, and that part of the tree will be excised. (The algorithm will not work if probabilities are left at zero.)
 
         public string SerializeResultsPrefixPlus(int scenario, int totalScenarios) => SerializeResultsPrefix + (totalScenarios > 0 ? scenario.ToString() : "");
 
