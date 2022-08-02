d769cb2692a8a60c2d7c9b6f1b9c5355d8c66b3b
mbabramo <mbabramo@yahoo.com>
2022-08-01 22:44:35 -0400

    DrawCircleCommand

--- ACESimBase/Util/Tikz/TikzPoint.cs
+++ ACESimBase/Util/Tikz/TikzPoint.cs
@@ -10,6 +10,8 @@ namespace ACESimBase.Util.Tikz
             return $"({x.ToSignificantFigures(5)},{y.ToSignificantFigures(5)})";
         }
 
+        public string DrawCircleCommand(string fillColor = "black", int points = 1) => $@"\draw[fill={fillColor}]({x},{y}) circle ({points} pt)";
+
         public TikzPoint WithXTranslation(double xShift) => new TikzPoint(x + xShift, y);
         public TikzPoint WithYTranslation(double yShift) => new TikzPoint(x, y + yShift);
         public TikzPoint WithTranslation(double xShift, double yShift) => new TikzPoint(x + xShift, y + yShift);
