bbcce18d98f7dcdb7fc1382eabb96df6aa2345e5
mbabramo <mbabramo@yahoo.com>
2025-09-21 11:44:33 -0400

    get timing on different executors

--- ACESimTest/GeneralizedVanillaTests.cs
+++ ACESimTest/GeneralizedVanillaTests.cs
@@ -71,25 +71,42 @@ namespace ACESimTest
                 Stopwatch s = new Stopwatch();
                 s.Start();
                 evolutionSettings.Unroll_ChunkExecutorKind = kind;
-                double[] addToResults = await DevelopStrategyAndGetUtilities(randomInformationSets, largerTree, evolutionSettings);
+                var developer = await Initialize(largerTree, evolutionSettings);
+                s.Stop();
+                long initializationTime = s.ElapsedMilliseconds;
+                s.Restart();
+                double[] addToResults = await RunAlgorithmAndGetUtilities(randomInformationSets, developer);
                 if (results.Any())
                     addToResults.SequenceEqual(results.First()).Should().BeTrue();
                 s.Stop();
-                Debug.WriteLine($"Time (random? {randomInformationSets} larger? {largerTree}) -- kind {kind} ==> {s.ElapsedMilliseconds}");
+                long runTime = s.ElapsedMilliseconds;
+                Debug.WriteLine($"Time (random? {randomInformationSets} larger? {largerTree}) -- kind {kind, 35} ==> initialization {initializationTime, 6} run for {evolutionSettings.TotalIterations} iterations {runTime, 6}");
             }
         }
 
         private static async Task<double[]> DevelopStrategyAndGetUtilities(bool randomInformationSets, bool largerTree, EvolutionSettings evolutionSettings)
         {
-            byte branching = largerTree ? (byte)4 : (byte)2; // signals, precaution powers, and precaution levels
-            var options = LitigGameOptionsGenerator.PrecautionNegligenceGame(largerTree, largerTree, branching, 1, branching, branching);
-            GeneralizedVanilla.ClearCache();
-            var developer = await GetGeneralizedVanilla(options, "TESTOPTIONS", evolutionSettings);
+            GeneralizedVanilla developer = await Initialize(largerTree, evolutionSettings);
+            double[] utilities = await RunAlgorithmAndGetUtilities(randomInformationSets, developer);
+            return utilities;
+        }
+
+        private static async Task<double[]> RunAlgorithmAndGetUtilities(bool randomInformationSets, GeneralizedVanilla developer)
+        {
             if (randomInformationSets)
                 RandomizeInformationSetProbabilities(developer);
             await developer.RunAlgorithm("TESTOPTIONS");
             double[] utilities = GetUtilities(developer);
             return utilities;
+        }
+
+        private static async Task<GeneralizedVanilla> Initialize(bool largerTree, EvolutionSettings evolutionSettings)
+        {
+            byte branching = largerTree ? (byte)8 /* DEBUG */ : (byte)2; // signals, precaution powers, and precaution levels
+            var options = LitigGameOptionsGenerator.PrecautionNegligenceGame(largerTree, largerTree, branching, 1, branching, branching);
+            GeneralizedVanilla.ClearCache();
+            var developer = await GetGeneralizedVanilla(options, "TESTOPTIONS", evolutionSettings);
+            return developer;
         }
     }
 }
